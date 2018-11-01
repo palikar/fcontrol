85fbb6f4b85e5017b26e4134528f3b6909d0d05d
Stanislav Arnaudov <stanislav_ts@abv.bg>
2018-11-01 03:49:33 +0100

    Implementing a 'nice' CLI interaface, writing out some of the README

--- fcontroll.js
+++ fcontroll.js
@@ -2,13 +2,53 @@ const fs = require('fs');
 const path = require('path');
 const login = require('facebook-chat-api');
 const readline = require('readline');
-const dispatch = require('./distributer.js');
+const minimist = require('minimist');
+const dispatch = require('./lib/distributer.js');
+
+let args = minimist(process.argv.slice(2), {  
+    string: ['email', 'pass', ],
+    boolean: ['cli', 'version', 'help', 'restore-state', 'save-state'],
+    alias: { v:'version', h:'help', p:'pass', e:'email' }
+});
 
 
-initFacebookChat('','',true, true);
 
+if (args.help) {    
+    console.log(`Description:
+    A tool that logs different events coming from your facebook messanger (messages, typing, presence, attachments)
+    Usage:
+        fcontrol [options]
 
+    Options:
+        -h, --help         print usage information and exit
+        -v, --version      show version info and exit
+        -cli               use the tool only with command line arguments
+        -e  --email        the email of your facebook acount 
+        -p  --pass         the password of your facebook acount`)
 
+    process.exit(1)
+}
+
+if (args.version) {
+    console.log("Version:1.0.0")
+    process.exit(1)
+}
+
+if(args.cli){
+    if(args['restore-state']){
+        if(!fs.existsSync(path.join(__dirname, '.appstate.json'))){
+            console.log("No .appstate.json file found, plaese log in first.")
+            process.exit(1)
+        }
+        initFacebookChat('', '', true, true);
+    }else{
+        if(!args.email || !args.pass){
+            console.log("You must provide email and password.")
+            process.exit(1)
+        }
+        initFacebookChat(args.email, args.pass, args['save-state'], false);   
+    }
+}
 
 var rl = readline.createInterface({
     input: process.stdin,
@@ -18,47 +58,48 @@ var rl = readline.createInterface({
 
 if (fs.existsSync(path.join(__dirname, '.appstate.json'))) {
     rl.question('Restore app state(y/n): ', (yn) => {
-        if (yn === 'y'){
-            initFacebookChat('', '', true);
+        if (yn.toLowerCase() === 'y' || yn.toLowerCase() === 'yes'){
+            initFacebookChat('', '', true, true);
         }else {
-            rl.question('Facebook Email adress: ', (name) => {
-                rl.stdoutMuted = true;
-                rl.query = 'Password : ';
-                rl.question(rl.query, (pass) => {
-                    console.log('\n');
-                    initFacebookChat(name, pass, false);
-                    rl.history = rl.history.slice(1);
-                    rl.close();
-                });
-                rl._writeToOutput = function _writeToOutput(stringToWrite) {
-                    if (rl.stdoutMuted)
-                        rl.output.write('\x1B[2K\x1B[200D'+rl.query+'['+((rl.line.length%2==1)?'=-':'-=')+']');
-                    else
-                        rl.output.write(stringToWrite);
-                };
-            });
-
+            promtLogin(rl)
         }
-
-
-        
     });
+}
+
+function promtLogin(rl){
+    rl.question('Facebook Email adress: ', (name) => {
+        rl.stdoutMuted = true;
+        rl.query = 'Password : ';
+        rl.question(rl.query, (pass) => {
+            console.log('\n');
+            initFacebookChat(name, pass, false);
+            rl.history = rl.history.slice(1);
+            rl.close();
+        });
+        rl._writeToOutput = function _writeToOutput(stringToWrite) {
+            if (rl.stdoutMuted)
+                rl.output.write('\x1B[2K\x1B[200D'+rl.query+'['+((rl.line.length%2==1)?'=-':'-=')+']');
+            else
+                rl.output.write(stringToWrite);
+        };
+    });
+
+
 
 }
 
 
 function initFacebookChat(email, pass, saveState, restoreState){
-
-
     var loginInfo;
     if (!restoreState){
         loginInfo ={email: email, password: pass};        
     }
     else{
-        loginInfo = {appState: JSON.parse(fs.readFileSync(path.join(__dirname,'.appstate.json'), 'utf8'))}; 
+        loginInfo = {appState: JSON.parse(fs.readFileSync(path.join(__dirname,'.appstate.json'), 'utf8'))};
     }
 
     login(loginInfo, (err, api) => {
+        if(err) return console.log(err)
         if (saveState)
             fs.writeFileSync('.appstate.json', JSON.stringify(api.getAppState()));
         
